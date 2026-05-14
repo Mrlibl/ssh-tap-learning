758223fa1ca40d02c52cade6913a0d8ae559b672
李宝龙 <baolong.li@percent.cn>
2026-05-14 14:19:38 +0800

    feat: ssh-tap-learning

---
+++ src/constant/sounds.js
@@ -0,0 +1,38 @@
+/*
+ * @Descripttion: 
+ * @Author: KB
+ * @Date: 2023-12-29 20:06:12
+ * @LastEditors: KB
+ * @LastEditTime: 2024-01-08 19:05:51
+ */
+
+const soundPath = [
+    require("./c-.mp3"),
+    require("./d-.mp3"),
+    require("./e-.mp3"),
+    require("./f-.mp3"),
+    require("./g-.mp3"),
+    require("./a-.mp3"),
+    require("./b-.mp3"),
+    require("./c-1.mp3"),
+    require("./d-1.mp3"),
+    require("./e-1.mp3"),
+    require("./f-1.mp3"),
+    require("./g-1.mp3"),
+    require("./a-1.mp3"),
+    require("./b-1.mp3"),
+    require("./c-2.mp3"),
+    require("./d-2.mp3"),
+    require("./e-2.mp3"),
+    require("./f-2.mp3"),
+    require("./g-2.mp3"),
+    require("./a-2.mp3"),
+    require("./b-2.mp3"),
+    require("./c3.mp3"),
+    require("./d3.mp3"),
+    require("./e3.mp3"),
+    require("./f3.mp3"),
+    require("./g3.mp3"),
+]
+
+export { soundPath }
